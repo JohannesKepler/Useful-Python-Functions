3571f743e80979203f0d87ac66852705c110721d
JohannesKepler <kwatzkwatz@gmail.com>
2015-03-06 16:00:33 -0500

    Create prime_sieve.py
    Use to quickly create a list of primes (or composites).

---
+++ prime_sieve.py
@@ -0,0 +1,32 @@
+def prime_sieve(limit):
+    # Initialize the primality list
+    a = [True] * limit
+    a[0] = a[1] = False
+
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            yield i
+            # Mark factors non-prime
+            for n in xrange(i, limit, i):
+                a[n] = False
+                
+def composite_sieve(limit):
+    # Initialize the primality list
+    a = [True] * limit
+    a[0] = a[1] = False
+
+    for (i, iscomposite) in enumerate(a):
+        if not iscomposite:
+            yield i
+            # Mark factors non-prime
+            for n in xrange(i, limit, i):
+                a[n] = False
+
+def main():
+    for prime in prime_sieve(100):
+        print prime
+    for composite in composite_sieve(100):
+        print composite
+
+if __name__ == '__main__':
+    main()
