42560c85f92b36b06ec1a1ce11bcbac10990ba95
JohannesKepler <kwatzkwatz@gmail.com>
2015-03-06 15:58:33 -0500

    Create gcd.py
    Greatest common denominator

---
+++ gcd.py
@@ -0,0 +1,17 @@
+'''Input (int1, int2); Output gcd as int.'''
+
+def gcd(a, b):
+    if a > b:
+        a, b = b, a
+    while True:
+        mod = b % a
+        if mod == 0:
+            return a
+        b, a = a, mod
+        
+def main():
+    input1, input2 = raw_input("Input two numbers, separated by a space: ").split()
+    print gcd(int(input1), int(input2))
+    
+if __name__ == '__main__':
+    main()
