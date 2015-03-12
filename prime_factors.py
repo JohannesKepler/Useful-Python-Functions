cc5b21b0234c32d17409347ef2c37398e595c5b7
Beck W <kwatzkwatz@gmail.com>
2015-03-12 08:55:16 -0400

    Add totient function
    Add a function to compute Euler's totient function phi(n), which uses prime factorization.

--- prime_factors.py
+++ prime_factors.py
@@ -1,6 +1,10 @@
+'''Algorithm for returning the prime factors of a number n.
+    Contains a function for computing Euler's totient function as well'''
+
 from math import sqrt
 
 def prime_factors(n):
+    '''Input a number n. Output is a list of prime factors.'''
     p_factors = []
     if n % 2 == 0:
         p_factors.append(2)
@@ -22,10 +26,27 @@ def prime_factors(n):
         
     return p_factors
     
+def totient(n, prime_facts = None):
+    '''Euler's totient function, phi(n), counts the numbers less than n
+    that are coprime with n. Input an integer. Optionally input list
+    of prime factors to avoid double work.'''
+    result = n
+    if not prime_facts:
+        prime_facts = prime_factors(n)
+    for prime in prime_facts:
+        result *= (prime-1)
+        result /= prime
+    return result
+
+    
 def main():
-    print "Returns list of prime factors."
+    '''Asks for a number as input and displays the list of prime factors
+    as well as the totient of the number.'''
+    print "Returns list of prime factors and phi(n)."
     user_input = int(raw_input("Input number: "))
-    print prime_factors(user_input)
+    result = prime_factors(user_input)
+    print result
+    print "Totient: %d" % totient(user_input, result)
     
 if __name__ == '__main__':
     main()
